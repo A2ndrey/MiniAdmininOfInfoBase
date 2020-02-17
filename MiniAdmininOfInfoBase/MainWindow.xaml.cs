0c9b16886c9e61555fa715c9c13f65d2ea38bdb0
AnDreW <andrew21ls@yandex.ru>
2020-02-17 21:52:05 +0300

    Добавьте файлы проекта.

---
+++ MiniAdmininOfInfoBase/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiniAdmininOfInfoBase
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+           
+        }
+
+        public void ConfirmExit()
+        {
+            MessageBoxResult result = MessageBox.Show("Вы уверены, что хотите выйти?",
+                                            "Выход",
+                                            MessageBoxButton.YesNo,
+                                            MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Settings SettingsWindows = new Settings();
+            SettingsWindows.Owner = this;
+            SettingsWindows.Show();
+         //   SettingsWindows.ShowViewModel();
+        }
+
+        private void ButtonHelp_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Properties.Settings.Default.OneC_path);
+            //  AboutBox1 lol = new AboutBox1();
+            //  lol.Owner = this;
+            //   lol.Show();
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.ConfirmExit();
+           // this.Close();
+        }
+    }
+}
---
+++ MiniAdmininOfInfoBase/Settings.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MiniAdmininOfInfoBase
+{
+    /// <summary>
+    /// Логика взаимодействия для Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public bool needToSave = false;
+
+        public Settings()
+        {
+            InitializeComponent();
+        }
+        //public void ShowViewModel()
+        //{
+        //    MessageBox.Show(ViewModel);
+        //}
+
+        public void OneC_PathIsEmpty(string Description)
+        {
+            MessageBox.Show(Description,"Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        private void Button_Click_Cancel(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_Save(object sender, RoutedEventArgs e)
+        {
+            if (OneC_Path.Text.Length == 0)
+            {
+                this.OneC_PathIsEmpty("Необходимо указать путь к исполняемому файлу");
+                return;
+            }
+
+            Properties.Settings.Default.OneC_path = OneC_Path.Text;
+            
+            if (UserName.Text.Length != 0)
+            {
+                Properties.Settings.Default.UserName = UserPassword.Password;
+            }
+
+            if (UserPassword.Password.Length !=0)
+            {
+                Properties.Settings.Default.Password = UserName.Text;
+            }
+
+            Properties.Settings.Default.Save();
+            this.Close();
+
+        }
+
+    }
+
+}
